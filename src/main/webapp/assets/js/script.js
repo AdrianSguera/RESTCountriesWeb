e4c573cb45e00526c261f37e46fa617b3396cd3f
Adrian_Sguera <adrianfsguera@gmail.com>
2024-02-23 20:23:37 +0100

    23/02/2024

--- src/main/webapp/assets/js/script.js
+++ src/main/webapp/assets/js/script.js
@@ -1,7 +1,27 @@
 window.onload = () => {
-    document.getElementsByClassName("region")[0].onclick = () => {
-        alert("hola");
-    };
+    let regions = document.getElementsByClassName("region");
+    for (var i = 0; i < regions.length; i++) {
+        regions[i].onclick = (event) => {
+            let region = event.currentTarget.innerHTML;
+            let flags = "";
+            fetch("https://restcountries.com/v3.1/region/" + region)
+                    .then(function (response) {
+                        return response.json();
+                    }).then(function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    flags += `<img class="flags" src="${data[i].flags.png}" alt="${data[i].name.common}"/>`;
+                }
+                document.getElementById("flags").innerHTML = flags;
+                // Capturar evento click en cada bandera
+                let flagElements = document.getElementsByClassName("flags");
+                for (let j = 0; j < flagElements.length; j++) {
+                    flagElements[j].onclick = (event) => {
+                        alert(event.currentTarget.alt);
+                    };
+                }
+            });
+        };
+    }
 };
 
 
